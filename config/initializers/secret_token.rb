7ad5286491ceb487d443d5d950198c15419fa2e0
MatthewTLackey <lackey.matt@gmail.com>
2013-12-07 22:43:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EcoD::Application.config.secret_key_base = '92776f2b1e93763858667b1ccccf906314dff6cb74c3fdc11da86eb0952db703f948f2038eb43195cddad5cdc58e7513c65324bfb2c264f40d9c76cf62cb4fe1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EcoD::Application.config.session_store :cookie_store, key: '_eco_D_session'
